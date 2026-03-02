048474b38264dbe4c367d727e46c1a03c4d3160e
Aref Ashrafi <aref.ashrafi@lansforsakringar.se>
2026-03-02 09:24:04 +0100

    change flower

--- app/components/FlowerBackground.tsx
+++ app/components/FlowerBackground.tsx
@@ -6,7 +6,7 @@ export default function FlowerBackground() {
   return (
     <div className='fixed inset-0 pointer-events-none' style={{ zIndex: 0 }}>
       <Image
-        src='/flower.webp'
+        src='/flower-compressed.webp'
         alt='Floral background'
         fill
         style={{ objectFit: 'cover', opacity: 0.7 }}
